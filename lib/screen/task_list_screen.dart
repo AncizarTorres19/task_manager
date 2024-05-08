f485a583dc91ddee6610d876df8f2c44eaf41dcb
AncizarTorres <ancizar_19_99@hotmail.com>
2024-05-07 19:27:16 -0500

    chore: Disable debug mode banner in app

--- lib/screen/task_list_screen.dart
+++ lib/screen/task_list_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../class/task.dart';
 
 class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+
   @override
   _TaskListScreenState createState() => _TaskListScreenState();
 }
@@ -26,12 +28,68 @@ class _TaskListScreenState extends State<TaskListScreen> {
   ];
 
   List<Task> filteredTasks = []; // Lista de tareas filtradas
-  TextEditingController searchController = TextEditingController(); // Controlador para el campo de búsqueda
+  TextEditingController searchController =
+      TextEditingController(); // Controlador para el campo de búsqueda
 
   @override
   void initState() {
-    filteredTasks = tasks; // Al principio, las tareas filtradas serán las mismas que las tareas originales
+    filteredTasks =
+        tasks; // Al principio, las tareas filtradas serán las mismas que las tareas originales
     super.initState();
+  }
+
+  void _showAddTaskDialog({Task? task}) {
+    String newTitle = task?.title ?? '';
+    String newDescription = task?.description ?? '';
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(task == null ? 'Agregar Tarea' : 'Actualizar Tarea'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: TextEditingController(text: newTitle),
+                onChanged: (value) => newTitle = value,
+                decoration: InputDecoration(labelText: 'Título'),
+              ),
+              TextField(
+                controller: TextEditingController(text: newDescription),
+                onChanged: (value) => newDescription = value,
+                decoration: InputDecoration(labelText: 'Descripción'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (newTitle.isNotEmpty && newDescription.isNotEmpty) {
+                  setState(() {
+                    if (task == null) {
+                      // Agregar nueva tarea
+                      tasks.add(Task(
+                        title: newTitle,
+                        description: newDescription,
+                        isCompleted: false,
+                      ));
+                    } else {
+                      // Actualizar tarea existente
+                      task.title = newTitle;
+                      task.description = newDescription;
+                    }
+                    _filterTasks(searchController.text);
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text(task == null ? 'Agregar' : 'Actualizar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _filterTasks(String query) {
@@ -46,55 +104,11 @@ class _TaskListScreenState extends State<TaskListScreen> {
     });
   }
 
-  void _showAddTaskDialog() {
-    String newTitle = '';
-    String newDescription = '';
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Agregar Tarea'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                onChanged: (value) => newTitle = value,
-                decoration: InputDecoration(labelText: 'Título'),
-              ),
-              TextField(
-                onChanged: (value) => newDescription = value,
-                decoration: InputDecoration(labelText: 'Descripción'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                if (newTitle.isNotEmpty && newDescription.isNotEmpty) {
-                  setState(() {
-                    tasks.add(Task(
-                      title: newTitle,
-                      description: newDescription,
-                      isCompleted: false,
-                    ));
-                    _filterTasks(searchController.text); // Filtrar tareas después de agregar una nueva
-                  });
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Text('Agregar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   void _removeTask(int index) {
     setState(() {
       tasks.removeAt(index);
-      _filterTasks(searchController.text); // Filtrar tareas después de eliminar una
+      _filterTasks(
+          searchController.text); // Filtrar tareas después de eliminar una
     });
   }
 
@@ -106,17 +120,9 @@ class _TaskListScreenState extends State<TaskListScreen> {
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16),
-            child: TextButton(
+            child: IconButton(
+              icon: Icon(Icons.add),
               onPressed: _showAddTaskDialog,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                  Color.fromARGB(255, 243, 240, 232),
-                ),
-              ),
-              child: Text(
-                'Agregar tarea',
-                style: TextStyle(color: Colors.black87),
-              ),
             ),
           ),
         ],
@@ -146,20 +152,33 @@ class _TaskListScreenState extends State<TaskListScreen> {
                       _removeTask(index);
                     },
                     background: Container(color: Colors.red),
-                    child: Card(
-                      elevation: 4, // Añadir elevación para sombra
-                      margin: EdgeInsets.all(8), // Espacio alrededor del Card
-                      child: ListTile(
-                        title: Text(filteredTasks[index].title),
-                        subtitle: Text(filteredTasks[index].description),
-                        trailing: Checkbox(
-                          value: filteredTasks[index].isCompleted,
-                          onChanged: (value) {
-                            setState(() {
-                              filteredTasks[index].isCompleted = value!;
-                            });
-                          },
-                        ),
+                    child: ListTile(
+                      title: Text(filteredTasks[index].title),
+                      subtitle: Text(filteredTasks[index].description),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: filteredTasks[index].isCompleted,
+                            onChanged: (value) {
+                              setState(() {
+                                filteredTasks[index].isCompleted = value!;
+                              });
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              _showAddTaskDialog(task: filteredTasks[index]);
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _removeTask(index);
+                            },
+                          ),
+                        ],
                       ),
                     ),
                   );
